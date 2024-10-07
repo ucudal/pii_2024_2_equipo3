52452be6c800ba93b3eaab73faa5988f52fcc3c8
Guillermo Gonzalez <guillermo.gonzalezt@correo.ucu.edu.uy>
2024-10-07 08:23:16 -0300

    Cambios guille

--- src/Library/Pokemon.cs
+++ src/Library/Pokemon.cs
@@ -12,7 +12,7 @@ public class Pokemon
         set { name = value; }
     }
 
-    ////////////////stats//////////////////////////////
+    ////////////////stats///////////////////////////////////
 
     private int maxhp;
     public int MaxHp
